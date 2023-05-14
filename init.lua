91db7fb5f39f158db2ca45093fd50b864170e58c
Kresna Laksana <baguskresnalaksana@gmail.com>
2023-05-15 04:12:25 +0800

    add & edit files
    add two files:
    1. heirline.lua
    2. user.lua
    
    edit two files:
    1. init.lua
    2. options.lua

--- init.lua
+++ init.lua
@@ -7,9 +7,9 @@ return {
     branch = "nightly", -- branch name (NIGHTLY ONLY)
     commit = nil, -- commit hash (NIGHTLY ONLY)
     pin_plugins = nil, -- nil, true, false (nil will pin plugins on stable only)
-    skip_prompts = false, -- skip prompts about breaking changes
+    skip_prompts = true, -- skip prompts about breaking changes
     show_changelog = true, -- show the changelog after performing an update
-    auto_quit = false, -- automatically quit the current session after a successful update
+    auto_quit = true, -- automatically quit the current session after a successful update
     remotes = { -- easily add new remotes to track
       --   ["remote_name"] = "https://remote_url.come/repo.git", -- full remote url
       --   ["remote2"] = "github_user/repo", -- GitHub user/repo shortcut,
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "astrodark",
+  colorscheme = "dracula",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
--- plugins/user.lua
+++ plugins/user.lua
@@ -1,6 +1,7 @@
 return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
+  'Mofiqul/dracula.nvim',
   -- "andweeb/presence.nvim",
   -- {
   --   "ray-x/lsp_signature.nvim",
